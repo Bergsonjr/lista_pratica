7c478c96e446fb07ae604c6b63b03e581776e435
Berg <bergsonjr@icloud.com>
2019-04-24 22:03:09 -0300

    initial commit

---
+++ grafos/grafos/Grafos.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grafos
+{
+    class Grafos
+    {
+        private bool isAdjacente(Vertice v1, Vertice v2)
+        {
+            return false;
+        }
+        private int getGrau(Vertice v1)
+        {
+            return 0;
+        }
+        private bool isIsolado(Vertice v1)
+        {
+            //if(v1.Proximos)
+            return false;
+        }
+        private bool isPendente(Vertice v1)
+        {
+            return false;
+        }
+        private bool isRegular()
+        {
+            return false;
+        }
+        private bool isNulo()
+        {
+            return true;
+        }
+        private bool isCompleto()
+        {
+            return true;
+        }
+        private bool isConexo()
+        {
+            return false;
+        }
+        private bool isEuleriano()
+        {
+            return true;
+        }
+        private bool isUnicursal()
+        {
+            return false;
+        }
+        private Grafos getComplementar()
+        {
+            return null;
+        }
+        private Grafos getAGMPrim(Vertice v1)
+        {
+            //deve retornar, para um grafo conexo, sua arvore geradora minima(Algoritmo de Prim)
+            return null;
+        }
+        private Grafos getAGMKruskal(Vertice v1)
+        {
+            //deve retornar, para um grafo conexo, sua arvore geradora minima(Algoritmo de Kruskal)
+            return null;
+        }
+
+        private int getCutVertices()
+        {
+            return 0;
+        }
+
+        //para grafos dirigidos
+        private int getGrauEntrada()
+        {
+            return 0;
+        }
+        private int getGrauSaida()
+        {
+            return 0;
+        }
+        private bool hasCiclo()
+        {
+            return true;
+        }
+    }
+}
---
+++ grafos/grafos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace grafos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathDirigido = "grafo_dirigido.txt";
+            //string pathNaoDirigido = "grafo_nao_dirigido.txt";
+
+            readFile(pathDirigido);
+            //readFile(pathNaoDirigido);
+            Console.ReadKey();
+        }
+        public static void readFile(string path)
+        {
+            StreamReader arq = new StreamReader(path);
+            string linha = "";
+            string[] separador; 
+            while (!arq.EndOfStream)
+            {
+                linha = arq.ReadLine();
+                separador = linha.Split(';');
+                Console.Write(separador[0]);
+            }
+        }
+    }
+}
